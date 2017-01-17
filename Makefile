1be2c72aa631952de0e69971db718317a0d944f3
Charly MZK <charlymrazeck@gmail.com>
2017-01-17 10:16:55 +0100

    Modifications IHM

--- Makefile
+++ Makefile
@@ -4,11 +4,11 @@ clean:
 	rm game dessine.o libdessine.a
 	
 game: gameLauncher.c game.c game.h libdessine.a
-	gcc -g gameLauncher.c -o game -L. -ldessine -lX11
+	gcc -g gameLauncher.c -o game -L. -ldessine -lX11 -lm
 
-libdessine.a: dessine.c dessine.h
+libdessine.a: dessine.c dessine.h 
 	gcc -g -c dessine.c
 	ar -r libdessine.a dessine.o
 	
 gamemake: benchmarkGame.c game.c
-	gcc -o gamebench benchmarkGame.c -L. -ldessine -lX11
+	gcc -o gamebench benchmarkGame.c -L. -ldessine -lX11 -lm
--- game.c
+++ game.c
@@ -1,8 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <math.h>
+#include <string.h>
 #include "game.h"
 #include "dessine.h"
+
+int taille_plateau;
+int turn = 0;
+
+
+void player_play(){
+	printf("ENtrée");
+	color(255,178,102); 
+	filled_rectangle(width_win()+5,0,300,30);
+
+	color(0,0,0); 
+	if(turn == 0){
+		string(width_win()+20,20,"Tour du joueur 1");
+	}else{
+		string(width_win()+20,20,"Tour du joueur 2");
+	}
+}
+
 
 /**
  * Mettre ici son code pour dessiner dans la fenetre
@@ -25,7 +45,11 @@ void draw_win()
 	
  	// -- Calcul du nombre de cases
 	box_size = width_win() / board_size;
+	box_size = round(box_size);
 
+	taille_plateau = 32;
+
+	printf("\n box size : %f",box_size);
 	// -- Dessin des lignes horizontales
 	for(i=0; i<width_win(); i+=box_size)
 		line(i,0,i, height_win());
@@ -33,8 +57,41 @@ void draw_win()
  	// -- Dessin des lignes verticales
 	for(i=0; i<height_win(); i+=box_size)
 		line(0,i,width_win(),i);	
+
+	
+		
 }
 
+
+
+int test_clicked(int coord,float taille_case){
+	float res = 0;
+	if(coord < taille_case){
+		coord = taille_case;
+	}else if(coord > width_win() - taille_case){
+		coord = width_win() - taille_case;
+	}else if(coord > height_win() - taille_case){
+		height_win() - taille_case;
+	}
+	  
+	res = round(coord/taille_case);
+	res = res * taille_case;  
+	return (int) res;
+}
+
+void drop_stone(int x, int y){
+	printf("drop");
+	if(turn == 0){
+		printf("couleur noire ");
+		turn = 1;
+		color(0,0,0);
+	}else{
+		printf("couleur blanche ");
+		turn = 0;
+		color(155,155,155);
+	}
+	filled_circle(x,y,5);
+}
 
 /**
  * on a cliqué a la souris:
@@ -43,13 +100,10 @@ void draw_win()
  */
 void mouse_clicked(int bouton, int x, int y)
 {
-	double i = x/30;
-	double j = y/30;
-	printf("\n Bouton %d presse au coord. %d,%d \n",bouton,x,y);
-	printf("\n %f",i);
-	printf("\n %f \n",j);
-	color(0,0,0);
-	filled_circle(x,y,5);
+	x = test_clicked(x,taille_plateau);
+	y = test_clicked(y,taille_plateau);  
+	player_play();
+	drop_stone(x,y);
 }
 
 
